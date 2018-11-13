839dce06fdd5bdf4626bd599ef7c6a5237f0192f
태휘 전 <20171270@vision.hoseo.edu>
2018-11-13 20:57:32 +0900

    PP12.Vector2D

--- GameFrameWork/PP01.HelloSDL/SDLGameObject.h
+++ GameFrameWork/PP01.HelloSDL/SDLGameObject.h
@@ -1,5 +1,6 @@
 #pragma once
 #include"GameObject.h"
+#include "Vector2D.h"
 
 class SDLGameObject : public GameObject
 {
@@ -11,11 +12,14 @@ public:
 	virtual void clean();
 
 protected:
-	int m_x;
-	int m_y;
+
 	int m_width;
 	int m_height;
 	int m_currentRow;
 	int m_currentFrame;
+	Vector2D m_position;
+	Vector2D m_velocity;
+	Vector2D m_acceleration;
+
 	std::string m_textureID;
 };
--- GameFrameWork/PP01.HelloSDL/main.cpp
+++ GameFrameWork/PP01.HelloSDL/main.cpp
@@ -4,8 +4,12 @@
 
 int main(int argc, char* argv[])
 {
+	const int FPS = 60;
+	const int DELAY_TIME = 1000.0f / FPS;
+
+	Uint32 frameStart, frameTime;
 	std::cout << "game init attempt...\n";
-	if (TheGame::Instance()->init("Chapter 10", 100, 100, 640, 480, false))
+	if (TheGame::Instance()->init("Chapter 11", 100, 100, 640, 480, false))
 	{
 		std::cout << "game init success!\n";
 		while (TheGame::Instance()->running())
@@ -22,6 +26,19 @@ int main(int argc, char* argv[])
 	}
 	std::cout << "game closing...\n";
 	TheGame::Instance()->clean();
-	return 0;
 
+	while (TheGame::Instance()->running())
+	{
+		frameStart = SDL_GetTicks();
+		TheGame::Instance()->handleEvents();
+		TheGame::Instance()->update();
+		TheGame::Instance()->render();
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameTime < DELAY_TIME)
+		{
+			SDL_Delay((int)(DELAY_TIME - frameTime));
+		}
+	}
+	return 0;
 }
