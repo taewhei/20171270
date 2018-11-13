50f932643bc664ca95c6367bcf9c6ad83ddc716f
태휘 전 <20171270@vision.hoseo.edu>
2018-11-13 13:00:05 +0900

    PP05.AnimationSprite

--- GameFrameWork/PP01.HelloSDL/Game.cpp
+++ GameFrameWork/PP01.HelloSDL/Game.cpp
@@ -3,53 +3,48 @@
 
 bool Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
-	m_bRunning = true;
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
-		if (fullscreen == true)
+		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_SHOWN);
+		if (m_pWindow != 0)
 		{
-			m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_FULLSCREEN);
-		}
-		else
-		{
-			m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_SHOWN);
-		}
-
-		if (m_pWindow != 0) {
 			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
-
 		}
+
+		m_bRunning = true;
+
+		SDL_Surface* pTempSurface = SDL_LoadBMP("assets/animate.bmp");
+		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
+
+		SDL_FreeSurface(pTempSurface);
+
+		SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
+		m_sourceRectangle.w = 128;
+		m_sourceRectangle.h = 82;
+
+		m_destinationRectangle.x = m_sourceRectangle.x = 0;
+		m_destinationRectangle.y = m_sourceRectangle.y = 0;
+		m_destinationRectangle.w = m_sourceRectangle.w;
+		m_destinationRectangle.h = m_sourceRectangle.h;
+
 
 	}
 	else {
-		return false;
+		return false; // sdl could not initialize
 	}
-
-	SDL_Surface* pTempSurface = SDL_LoadBMP("assets/rider.bmp");
-
-	m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
-
-	SDL_FreeSurface(pTempSurface);
-
-	SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
-
 	return true;
+
 }
 void Game::update()
 {
-
+	m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
 }
 void Game::render()
 {
-	m_destinationRectangle.x = m_sourceRectangle.x = 0;
-	m_destinationRectangle.y = m_sourceRectangle.y = 0;
-	m_destinationRectangle.w = m_sourceRectangle.w;
-	m_destinationRectangle.h = m_sourceRectangle.h;
-
 	SDL_RenderClear(m_pRenderer);
-
 	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
 	SDL_RenderPresent(m_pRenderer);
+
 }
 void Game::clean()
 {
