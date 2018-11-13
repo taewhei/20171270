da806a43d40f3de53b288caf556f55d85cc7a868
태휘 전 <20171270@vision.hoseo.edu>
2018-11-13 13:11:31 +0900

    PP07.TextureManager

--- GameFrameWork/PP01.HelloSDL/Game.cpp
+++ GameFrameWork/PP01.HelloSDL/Game.cpp
@@ -13,21 +13,7 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 		}
 
 		m_bRunning = true;
-
-		SDL_Surface* pTempSurface = IMG_Load("Assets/animate-alpha.png");
-
-		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
-
-		SDL_FreeSurface(pTempSurface);
-
-		SDL_QueryTexture(m_pTexture, NULL, NULL, &m_sourceRectangle.w, &m_sourceRectangle.h);
-		m_sourceRectangle.w = 128;
-		m_sourceRectangle.h = 82;
-
-		m_destinationRectangle.x = m_sourceRectangle.x = 0;
-		m_destinationRectangle.y = m_sourceRectangle.y = 0;
-		m_destinationRectangle.w = m_sourceRectangle.w;
-		m_destinationRectangle.h = m_sourceRectangle.h;
+		m_textureManager.load("assets/animate-alpha.png", "animate", m_pRenderer);
 
 		SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255);
 	}
@@ -39,14 +25,15 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 }
 void Game::update()
 {
-	m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
 }
 void Game::render()
 {
 	SDL_RenderClear(m_pRenderer);
-	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+	m_textureManager.draw("animate", 0, 0, 128, 82, m_pRenderer);
+	m_textureManager.drawFrame("animate", 100, 100, 128, 82, 1,
+		m_currentFrame, m_pRenderer);
 	SDL_RenderPresent(m_pRenderer);
-
 }
 void Game::clean()
 {
--- GameFrameWork/PP01.HelloSDL/Game.h
+++ GameFrameWork/PP01.HelloSDL/Game.h
@@ -1,6 +1,6 @@
 #pragma once
-
-#include "SDL.h"
+#include <SDL.h>
+#include "TextureManager.h"
 
 class Game
 {
@@ -9,15 +9,15 @@ public:
 	~Game() {}
 	bool init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
 	void render();
-	void update();
+
 	void handleEvents();
 	void clean();
+	void update();
 	bool running() { return m_bRunning; }
 private:
 	SDL_Window * m_pWindow;
 	SDL_Renderer* m_pRenderer;
 	bool m_bRunning;
-	SDL_Texture* m_pTexture;
-	SDL_Rect m_sourceRectangle;
-	SDL_Rect m_destinationRectangle;
+	int m_currentFrame;
+	TextureManager m_textureManager;
 };
