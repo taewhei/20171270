faa7f6d476f4968910f1bd9f77c550838b5019a0
태휘 전 <20171270@vision.hoseo.edu>
2018-11-13 12:44:58 +0900

    PP03.GameClass

---
+++ GameFrameWork/PP01.HelloSDL/Game.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include "Game.h"
+
+bool Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
+{
+	bool g_bRunning = true;
+
+	SDL_Window*g_pWindow = 0;
+	SDL_Renderer*g_pRenderer = 0;
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+		if (fullscreen == true)
+		{
+			g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_FULLSCREEN);
+		}
+		else
+		{
+			g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_SHOWN);
+		}
+
+		if (g_pWindow != 0) {
+			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+
+		}
+
+	}
+	else {
+		return 1;
+	}
+	SDL_Rect rect = { 10,10,40,40 };
+
+	SDL_SetRenderDrawColor(g_pRenderer, 255, 0, 0, 255);
+
+	SDL_Delay(5000);
+	SDL_Quit();
+	return true;
+}
+void Game::update()
+{
+
+}
+void Game::render()
+{
+	SDL_RenderClear(m_pRenderer);
+	SDL_RenderPresent(m_pRenderer);
+}
+void Game::clean()
+{
+	std::cout << "cleaning game\n";
+	SDL_DestroyWindow(m_pWindow);
+	SDL_DestroyRenderer(m_pRenderer);
+	SDL_Quit();
+}
+void Game::handleEvents()
+{
+	SDL_Event event;
+	if (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			m_bRunning = false;
+			break;
+		default:
+			break;
+		}
+	}
+}
--- GameFrameWork/PP01.HelloSDL/main.cpp
+++ GameFrameWork/PP01.HelloSDL/main.cpp
@@ -1,44 +1,16 @@
-#include<SDL.h>
-
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
-
-bool g_bRunning = false;
-
-bool init(const char*title, int xpos, int ypos, int width, int height, int flags)
-{
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-	{
-		g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-		if (g_pWindow != 0)
-		{
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
-	}
-	else {
-		return false;
-	}
-	return true;
-}
-void render()
-{
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-	SDL_RenderClear(g_pRenderer);
-	SDL_RenderPresent(g_pRenderer);
-}
+#include"Game.h"
+Game* g_game = 0;
 int main(int argc, char* argv[])
 {
-	if (init("Chapter1 : Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN)) {
-		g_bRunning = true;
+	g_game = new Game();
+	g_game->init("Chapter1", 100, 100, 640, 480, false);
+	while (g_game->running())
+	{
+		g_game->handleEvents();
+		g_game->update();
+		g_game->render();
 	}
-	else {
-		return 1;
-	}
-	while (g_bRunning) {
-		render();
-	}
-	SDL_Quit();
+	g_game->clean();
 	return 0;
 }
-
 
